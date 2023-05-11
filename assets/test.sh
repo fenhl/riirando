b47992873a9689392396ee1120f06083cdfdcb5d
Fenhl <fenhl@fenhl.net>
2023-05-11 00:03:51 +0000

    Better folder structure for generated files

--- assets/test.sh
+++ assets/test.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-cargo run < ~/games/zelda/oot/oot-ntscu-1.0.z64 > default.zpf
+mkdir -p assets/generated
+cargo run < ~/games/zelda/oot/oot-ntscu-1.0.z64 > assets/generated/default.zpf
 python3 /opt/git/github.com/TestRunnerSRL/OoT-Randomizer/master/OoTRandomizer.py --settings=- <<EOF
 {
     "generate_from_file": true,
     "rom": "/Users/fenhl/games/zelda/oot/oot-ntscu-1.0.z64",
-    "patch_file": "/Users/fenhl/games/zelda/oot/riirando/default.zpf"
+    "patch_file": "/Users/fenhl/games/zelda/oot/riirando/assets/generated/default.zpf",
+    "output_dir": "/Users/fenhl/games/zelda/oot/riirando/assets/generated"
 }
 EOF
-rm default.zpf
